d6943a4bf402a60a82436bff503ae604f7dca64a
Mathieu Gascon-Lefebvre <mathieu.gascon@gsoft.com>
2021-03-31 14:25:16 -0400

    Updated to latest yelp detect_secrets version.

--- github_secret_finder/analysis/patch_analyzer.py
+++ github_secret_finder/analysis/patch_analyzer.py
@@ -1,7 +1,7 @@
-from detect_secrets.core.constants import VerifiedResult
-from detect_secrets.core.usage import PluginOptions
-from detect_secrets.plugins.common import initialize
-from unidiff import PatchSet, UnidiffParseError
+from detect_secrets.core.plugins.util import get_mapping_from_secret_type_to_class
+from detect_secrets import SecretsCollection
+from detect_secrets.settings import transient_settings
+from unidiff import PatchSet
 
 from .blacklist_matcher import BlacklistMatcher
 from .Secret import Secret
@@ -9,44 +9,36 @@ from .Secret import Secret
 
 class PatchAnalyzer(object):
     def __init__(self, blacklist_file):
-        active_plugins = {}
-        for plugin in PluginOptions.all_plugins:
-            related_args = {}
-            for related_arg_tuple in plugin.related_args:
-                flag_name, default_value = related_arg_tuple
-                related_args[flag_name[2:].replace("-", "_")] = default_value
-
-            active_plugins[plugin.classname] = related_args
-
-        self._plugins = initialize.from_parser_builder(
-            active_plugins,
-            exclude_lines_regex=None,
-            automaton=False,
-            should_verify_secrets=True)
-
         self._blacklist = BlacklistMatcher(blacklist_file)
 
-    def find_secrets(self, patch_text):
-        try:
-            patch = PatchSet.from_string(patch_text)
-        except UnidiffParseError:
-            return
+    def find_secrets(self, diff):
+        changes = None
 
-        for p in self._plugins:
-            for patch_file in patch:
-                for chunk in patch_file:
-                    for line in chunk.target_lines():
-                        if line.is_added:
-                            l = line.value.strip()
-                            for k in p.analyze_line(l, line.target_line_no, patch_file.path):
-                                if len(k.secret_value) < 6:
-                                    continue  # Ignore small secrets to reduce false positives.
+        secrets_collection = SecretsCollection()
+        with transient_settings({'plugins_used': [{'name': plugin_type.__name__} for plugin_type in get_mapping_from_secret_type_to_class().values()]}) as settings:
+            settings.disable_filters(
+                'detect_secrets.filters.common.is_invalid_file',
+            )
+            secrets_collection.scan_diff(diff)
 
-                                if self._blacklist.is_blacklisted(l, k.filename, k.secret_value):
-                                    continue
+        for file_name, secret in secrets_collection:
+            if len(secret.secret_value) < 6:
+                continue  # Ignore small secrets to reduce false positives.
 
-                                # detect_secrets sometimes return a lowercase version of the secret. Find the real string.
-                                secret_index = l.lower().find(k.secret_value.lower())
-                                secret_value = l[secret_index:secret_index + len(k.secret_value)]
+            # Only parse the diff if at least one secret was found.
+            if not changes:
+                patch_set = PatchSet.from_string(diff)
+                changes = {}
+                for patch_file in patch_set:
+                    lines = dict((line.target_line_no, line.value.strip()) for chunk in patch_file for line in chunk.target_lines() if line.is_added)
+                    changes[patch_file.path] = lines
 
-                                yield Secret(k.type, k.filename, k.lineno, secret_value, l, p.verify(secret_value, content=l) == VerifiedResult.VERIFIED_TRUE)
+            line = changes[secret.filename][secret.line_number]
+            if self._blacklist.is_blacklisted(line, file_name, secret.secret_value):
+                continue
+
+            # detect_secrets sometimes return a lowercase version of the secret. Find the real string.
+            secret_index = line.lower().find(secret.secret_value.lower())
+            secret_value = line[secret_index:secret_index + len(secret.secret_value)]
+
+            yield Secret(secret.type, secret.filename, secret.line_number, secret_value, line, secret.is_verified)
